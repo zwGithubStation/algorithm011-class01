fdc65e8ea5b10228ebcca99d8f7ae9eac0d18715
zwGithubStation <zwp_sz_pro@163.com>
2020-07-20 20:24:55 +0800

    22 problems

--- Week_04/class_example_problems/generateParenthesis_22/generateParenthesis.cpp
+++ Week_04/class_example_problems/generateParenthesis_22/generateParenthesis.cpp
@@ -6,60 +6,59 @@
 
    baseline solve :
 
-   compile: g++ -lstdc++ -fsanitize=address -fno-omit-frame-pointer -O1 -g levelOrder.cpp  -o levelOrder
+   compile: g++ -std=c++11 -fsanitize=address -fno-omit-frame-pointer -O1 -g generateParenthesis.cpp -o generateParenthesis
  */
 
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 #include <vector>
 #include <cstdlib>
 #include <queue>
 
 using namespace std;
 
- struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
- 
 class Solution {
 public:
-    vector<vector<int > > levelOrder(TreeNode* root) {
-		int cnt = 0;
-		vector<vector<int > > res;
-
-		queue<TreeNode* > nodesQueue;
-		nodesQueue.push(root);
-
-		while (!nodesQueue.empty())
-		{
-			vector<int > curRes;
-			cnt = nodesQueue.size();
-			TreeNode* curNode;
-			while (cnt--)
-			{
-				curNode = nodesQueue.front();
-				nodesQueue.pop();
-				curRes.push_back(curNode->val);
-				if (curNode->left)
-					nodesQueue.push(curNode->left);
-				if (curNode->right)
-					nodesQueue.push(curNode->right);	
-			}
-			res.push_back(curRes);
-		}
-		return res;
+    vector<string> generateParenthesis(int n) {
+		vector<string> ret;
+		string curStr;
+		curStr[0] = '(';
+		dfs(ret, curStr, n, 1, 1);
+		return ret;
     }
+
+	void dfs(vector<string> &result, string &curStr, int n, int level, int left_cnt)
+	{
+		if (level == 2*n-1)
+		{
+			curStr[level] = ')';
+			result.push_back(curStr);
+			return;
+		}
+
+		int right_cnt = level + 1 - left_cnt;
+
+		if (right_cnt == left_cnt)
+		{
+			curStr[level] = '(';
+			dfs(result, curStr, n, level+1, left_cnt+1);
+			return;
+		}
+
+		if (left_cnt == n)
+		{
+			curStr[level] = ')';
+			dfs(result, curStr, n, level+1, left_cnt);
+			return;
+		}
+
+		curStr[level] = '(';
+		dfs(result, curStr, n, level+1, left_cnt+1);
+
+		curStr[level] = ')';
+		dfs(result, curStr, n, level+1, left_cnt);
+
+		return;
+	}
 };
 
 int main()
